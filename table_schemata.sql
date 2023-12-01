09179ce082ea678a99119f3af99c9fd12871c39d
Quentin O'Neal <qbicle14@gmail.com>
2023-12-01 01:38:45 -0500

    adding separate queries and tables files

---
+++ table_schemata.sql
@@ -0,0 +1,49 @@
+-- Drop tables in reverse order of dependencies
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS 
+
+
+CREATE TABLE titles(
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(30)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(20) PRIMARY KEY,
+    dept_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR (1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(20),
+    emp_no INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
